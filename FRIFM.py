4ea428ff4a086de4775a20ffb31dc44490ebe0ab
Brogan, Joel <broganjr@ornl.gov>
2021-03-03 16:53:31 -0500

    first commit

---
+++ FRIFM.py
@@ -0,0 +1,34 @@
+'''
+Implementation of FRIFM : https://tsapps.nist.gov/publication/get_pdf.cfm?pub_id=890059
+Created on March 3, 2021 at Oak Ridge National Laboratory
+@author: Joel Brogan
+'''
+
+import dlib
+import numpy as np
+from skimage.filters import sobel 
+from skimage.transform import rescale
+import skimage.io as skio
+
+import os
+import sys
+
+def FRIFM(im,det,standardWidth=150):
+    chip = im[d.top():d.bottom(),d.left():d.right()]
+    chip_width = chip.shape[1]
+    scale_factor = standardWidth/chip_width
+    chip = rescale(chip,scale_factor,anti_aliasing=False)
+    FRIFM = sobel(chip).mean()
+    return FRIFM
+    
+if __name__ == '__main__': 
+    impath = sys.argv[1]
+    detector = dlib.get_frontal_face_detector()
+    im = (skio.imread(impath,1)*255).astype(np.uint8)
+    
+    dets = detector(im)
+    FRIFM_array = []
+    for d in dets:
+        FRIFM_array.append(FRIFM(im,d))
+    print("Number of faces detected: ", len(FRIFM_array))
+    print("FRIFM Values:",FRIFM_array)
